233edcca26710e197e60e5c3f5c4dddc8c347450
Rait Pravleen Singh <36415182+lllSniperlyfelll@users.noreply.github.com>
2020-04-27 17:40:46 +0530

    Chnaged files path to dynamic

--- indexpage_writer.py
+++ indexpage_writer.py
@@ -1,3 +1,5 @@
+import os
+
 
 class html_writer():
 
@@ -11,7 +13,7 @@ class html_writer():
 		self.dropdown_font_color = 'text-light'
 		self.footer_font_color='text-warning'
 		
-		self.body_bgcolor = 'bg-primary'
+		self.body_bgcolor = 'bg-light'
 		self.navbar_bgcolor = 'bg-dark'
 		self.dropdown_bgcolor = 'bg-danger'
 		self.card_bgcolor='bg-dark'
@@ -139,7 +141,7 @@ class html_writer():
 
 	def file_writer(self):
 
-		f = open("E:/DELL/Documents/WebCustomizer\html_pages/index.html","w")
+		f = open(os.path.abspath("html_pages/index.html"),"w")
 		head_tag = self.header_writer()
 		f.write(head_tag)
 		f.close()
